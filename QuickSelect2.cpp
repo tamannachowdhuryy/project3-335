b19b822f99be36a368e0eee8ad8a9673605e2714
Tamanna Chowdhury <tamanna.chowdhury610@gmail.com>
2024-05-07 16:56:48 -0400

    updated w pdf

--- QuickSelect2.cpp
+++ QuickSelect2.cpp
@@ -101,10 +101,10 @@ void quickSelect2(const std::string& header, std::vector<int> data) {
         insertionSort(data, 0, n - 1);
     } else {
         // Use quickSelect for larger input sizes
-        std::set<int> keys = {0, (n + 3) / 4 - 1, (n + 1) / 2 - 1, 3 * (n + 1) / 4 - 1, n - 1};
+        std::set<int> keys = {0, (n + 3) / 4 - 1, (n + 1) / 2 - 1, 3 * (n + 1) / 4 - 1, n - 1}; // 
         quickSelect(data, 0, n - 1, keys);
     }
-   
+    //getting all the data and printing it out 
     std::cout << header << std::endl;
     std::cout << "Min: " << data[0] << std::endl;
     std::cout << "P25: " << data[(n + 3) / 4 - 1] << std::endl;
